57d27c01d8569169401a418b26d482a5f0ccacf1
Ngoubou <phuong@autonomous.nyc>
2018-08-17 14:05:01 +0700

    bignumber.js-4.1.0

--- src/transaction/trustline.ts
+++ src/transaction/trustline.ts
@@ -1,5 +1,5 @@
 import * as _ from 'lodash'
-import BigNumber from 'bignumber.js'
+import BigNumber from 'bignumber.js-4.1.0'
 import * as utils from './utils'
 const validate = utils.common.validate
 const trustlineFlags = utils.common.txFlags.TrustSet
